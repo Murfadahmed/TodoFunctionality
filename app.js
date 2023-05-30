c17de49ac84933ac90f46252ad4e90c0ef81cf6e
murfadahmed <malikmurfad361@gmail.com>
2023-05-30 14:31:15 +0500

    delete

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 let inputText = document.querySelector('.text')
 let inputBtn = document.querySelector('.button')
-    let itemList = document.querySelector('.list ul')
-    
+let itemList = document.querySelector('.list ul')
 
-    
-    
-    inputBtn.addEventListener('click',(item)=>{
-        if(inputText.value != ""){
 
-            item.preventDefault()
 
-            let myItem = document.createElement('li');
-            
-            console.log(myItem)
-            
-            myItem.innerHTML = inputText.value;
 
-            itemList.appendChild(myItem)
+inputBtn.addEventListener('click', (item) => {
+    if (inputText.value != "") {
 
-            const mySpan =  document.createElement('span')
-            mySpan.innerHTML = "x"
-            console.log(mySpan)
-            myItem.appendChild(mySpan)
-        } 
+        item.preventDefault()
 
-         const close = document.querySelectorAll('span')
-        // console.log(close)
-        for(let i=0; i<close.length ;i++)
-        {
-            close[i].addEventListener('click',()=>{
-                // console.log(i)
-                console.log(close[i])
-                close[i].parentElement.style.display ="none"
-            })
-        }
-        inputText.value = ""
-    })
+        let myItem = document.createElement('li');
+
+        console.log(myItem)
+
+        myItem.innerHTML = inputText.value;
+
+        itemList.appendChild(myItem)
+
+        const mySpan = document.createElement('button')
+        mySpan.setAttribute('class','btndelete')
+        mySpan.innerHTML = "delete"
+        console.log(mySpan)
+        myItem.appendChild(mySpan)
+    }
+
+    const close = document.querySelectorAll('button')
+    // console.log(close)
+    for (let i = 0; i < close.length; i++) {
+        close[i].addEventListener('click', () => {
+            // console.log(i)
+            console.log(close[i])
+            close[i].parentElement.style.display = "none"
+        })
+    }
+    inputText.value = ""
+})
